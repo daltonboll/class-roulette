38bf9a91413292576fc5ad03df120fa84e026a1b
Dalton Boll <daltonboll@gmail.com>
2015-04-27 21:49:23 -0700

    Add department attribute to Course model
    Add additonal form input for course department upon editing and creating courses.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -88,6 +88,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name)
+      params.require(:course).permit(:department, :name)
     end
 end
