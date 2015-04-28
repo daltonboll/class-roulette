a74fe76c09331f67ef534118f65a72b65eb9edfc
Dalton Boll <daltonboll@gmail.com>
2015-04-27 22:14:20 -0700

    Fix many inconsistencies across redirect routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428020401) do
+ActiveRecord::Schema.define(version: 20150428050845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150428020401) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "department"
   end
 
   create_table "lectures", force: :cascade do |t|
